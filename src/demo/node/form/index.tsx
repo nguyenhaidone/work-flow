3ff3b48ed297da33f0608d71ef79e5446831f81a
Nguyen Hai Dang <dang682000@gmail.com>
2022-09-23 15:24:22 +0700

    fix-build

--- src/demo/node/form/index.tsx
+++ src/demo/node/form/index.tsx
@@ -1,28 +1,27 @@
 import React, { useState, useContext } from "react";
-import AddButton, { BuilderContext, useAction } from "react-flow-builder";
+import { BuilderContext, useAction } from "react-flow-builder";
 import FlowBuilder, {
   NodeContext,
   INode,
   IRegisterNode,
 } from "react-flow-builder";
-import ConfigForm from "./ConfigForm";
 
 import "./index.css";
 
 const StartNodeDisplay: React.FC = () => {
-  const node = useContext(NodeContext);
-  const {
-    nodes,
-    readonly,
-    registerNodes,
-    beforeNodeClick,
-    beforeAddConditionNode,
-    dragType,
-    // DropComponent = DropButton,
-    showPracticalBranchNode,
-    showPracticalBranchRemove,
-    sortable,
-  } = useContext(BuilderContext);
+  // const node = useContext(NodeContext);
+  // const {
+  //   nodes,
+  //   readonly,
+  //   registerNodes,
+  //   beforeNodeClick,
+  //   beforeAddConditionNode,
+  //   dragType,
+  //   // DropComponent = DropButton,
+  //   showPracticalBranchNode,
+  //   showPracticalBranchRemove,
+  //   sortable,
+  // } = useContext(BuilderContext);
   // console.log('Node context:');
   // console.log(node);
   // console.log('--------------------------------------------------------');
@@ -224,55 +223,55 @@ const registerNodes: IRegisterNode[] = [
   },
 ];
 
-const defaultNodes = [
-  {
-    id: "node-0d9d4733-e48c-41fd-a41f-d93cc4718d97",
-    type: "start",
-    name: "start",
-    path: ["0"],
-  },
-  {
-    id: "node-b2ffe834-c7c2-4f29-a370-305adc03c010",
-    type: "branch",
-    name: "Branch node",
-    children: [
-      {
-        id: "node-cf9c8f7e-26dd-446c-b3fa-b2406fc7821a",
-        type: "condition",
-        name: "Condition node",
-        children: [
-          {
-            id: "node-f227cd08-a503-48b7-babf-b4047fc9dfa5",
-            type: "node",
-            name: "Normal node",
-            path: ["1", "children", "0", "children", "0"],
-          },
-        ],
-        path: ["1", "children", "0"],
-      },
-      {
-        id: "node-9d393627-24c0-469f-818a-319d9a678707",
-        type: "condition",
-        name: "Condition node",
-        children: [],
-        path: ["1", "children", "1"],
-      },
-    ],
-    path: ["1"],
-  },
-  {
-    id: "node-972401ca-c4db-4268-8780-5607876d8372",
-    type: "node",
-    name: "Normal node",
-    path: ["2"],
-  },
-  {
-    id: "node-b106675a-5148-4a2e-aa86-8e06abd692d1",
-    type: "end",
-    name: "end",
-    path: ["3"],
-  },
-];
+// const defaultNodes = [
+//   {
+//     id: "node-0d9d4733-e48c-41fd-a41f-d93cc4718d97",
+//     type: "start",
+//     name: "start",
+//     path: ["0"],
+//   },
+//   {
+//     id: "node-b2ffe834-c7c2-4f29-a370-305adc03c010",
+//     type: "branch",
+//     name: "Branch node",
+//     children: [
+//       {
+//         id: "node-cf9c8f7e-26dd-446c-b3fa-b2406fc7821a",
+//         type: "condition",
+//         name: "Condition node",
+//         children: [
+//           {
+//             id: "node-f227cd08-a503-48b7-babf-b4047fc9dfa5",
+//             type: "node",
+//             name: "Normal node",
+//             path: ["1", "children", "0", "children", "0"],
+//           },
+//         ],
+//         path: ["1", "children", "0"],
+//       },
+//       {
+//         id: "node-9d393627-24c0-469f-818a-319d9a678707",
+//         type: "condition",
+//         name: "Condition node",
+//         children: [],
+//         path: ["1", "children", "1"],
+//       },
+//     ],
+//     path: ["1"],
+//   },
+//   {
+//     id: "node-972401ca-c4db-4268-8780-5607876d8372",
+//     type: "node",
+//     name: "Normal node",
+//     path: ["2"],
+//   },
+//   {
+//     id: "node-b106675a-5148-4a2e-aa86-8e06abd692d1",
+//     type: "end",
+//     name: "end",
+//     path: ["3"],
+//   },
+// ];
 
 const inputNodes = [
   {
